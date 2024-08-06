54a1cd9fa720ac99db4168d95a1752a05851729b
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2024-08-06 13:53:29 -0400

    correction style scroll-arrow

--- client/src/views/ProjectsView.vue
+++ client/src/views/ProjectsView.vue
@@ -1,8 +1,35 @@
 <script setup>
+import ScrollArrow from '@/components/ScrollArrow.vue'
+
 </script>
 
 <template>
   <main>
-    <h1>Projects</h1>
+    <section class="page-container">
+      <h1>Projects</h1>
+      <div class="projects-container">
+        <div class="project-card">
+          <figure class="project-card-image">
+            <img src="https://via.placeholder.com/600" alt="Project Title" />
+          </figure>
+          <div class="project-card-info">
+            <h2>Project Title</h2>
+            <p>Project Description</p>
+            <a href="#">View Project</a>
+          </div>
+        </div>
+        <div class="project-card">
+          <figure class="project-card-image">
+            <img src="https://via.placeholder.com/600" alt="Project Title" />
+          </figure>
+          <div class="project-card-info">
+            <h2>Project Title</h2>
+            <p>Project Description</p>
+            <a href="#">View Project</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <ScrollArrow />
   </main>
 </template>

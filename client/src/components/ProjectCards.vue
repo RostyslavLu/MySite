eacbd726b8b1519715407cccb26e204570adb1d2
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2024-08-15 19:29:47 -0400

    language files improvement

--- client/src/components/ProjectCards.vue
+++ client/src/components/ProjectCards.vue
@@ -1,7 +1,8 @@
 <script setup>
 import ProjectCard from './ProjectCard.vue'
 import { useI18n } from 'vue-i18n'
-
+import videoAppVino from '@/assets/video/app-vino-web.mp4'
+import imageAppVino from '@/assets/img/photos/images-app-vino/img5.jpg'
 const { t } = useI18n()
 </script>
 
@@ -10,9 +11,9 @@ const { t } = useI18n()
     <ProjectCard
       :title="t('projects.projectItems[0].title')"
       :description="t('projects.projectItems[0].description')"
-      :img="t('projects.projectItems[0].media')"
+      :img="imageAppVino"
       :alt="t('projects.projectItems[0].alt')"
-      :videoFile="t('projects.projectItems[0].videoFile')"
+      :videoFile="videoAppVino"
       />
   </div>
 </template>

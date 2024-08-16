ca52b68841fae4ebc39b99f9f005b3eb74bb383b
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2024-08-15 22:19:36 -0400

    dropdown improvement

--- client/src/components/LanguageSwitcher.vue
+++ client/src/components/LanguageSwitcher.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import enFlag from '@/assets/img/logo/en.png'
 import frFlag from '@/assets/img/logo/fr.png'
@@ -9,9 +9,10 @@ const { locale } = i18n
 const availableLocales = ref(['en', 'fr'])
 
 const selectedLocale = ref(locale.value)
-
+const dropdownOpen = ref(false)
 watch(selectedLocale, (newValue) => {
   i18n.locale.value = newValue
+  dropdownOpen.value = false
 })
 
 const getFlagImage = (locale) => {
@@ -25,15 +26,21 @@ const getFlagImage = (locale) => {
   }
 }
 
+const filteredLocales = computed(() => {
+  return availableLocales.value.filter(l => l !== selectedLocale.value)
+})
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value
+}
 </script>
 
 <template>
-  <div class="dropdown">
-    <button class="dropbtn">
+  <div :class="['dropdown', { open: dropdownOpen }]">
+    <button class="dropbtn" @click="toggleDropdown">
       <img :src="getFlagImage(selectedLocale)" alt="Flag" width="36px" height="24px" loading="lazy" />
     </button>
     <div class="dropdown-content">
-      <a v-for="locale in availableLocales" :key="locale" @click="selectedLocale = locale">
+      <a v-for="locale in filteredLocales" :key="locale" @click="selectedLocale = locale">
         <img :src="getFlagImage(locale)" alt="Flag" width="36px" height="24px" loading="lazy" />
       </a>
     </div>
--- server/index.js
+++ server/index.js
@@ -15,22 +15,31 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the server' })
 })
-
+/* mail sending */
 app.post('/send-mail', (req, res) => {
     const { name, email, message } = req.body
+    /* send email to the admin */
     const emailOptions = {
         from: process.env.MAIL_FROM_ADDRESS,
         to: process.env.MAIL_USERNAME,
-        cc: email,
         subject: `Message from ${name}`,
         text: `Message from ${name} ${email}`,
         html: `<p>Message from ${name} ${email}:</p>
             <p>${message}</p>`
     }
     emailService.sendEmail(emailOptions)
-
+    /* send confirmation email to the sender */
+    const confirmationEmailOptions = {
+        from: process.env.MAIL_FROM_ADDRESS,
+        to: email,
+        subject: 'Confirmation',
+        text: 'Thank you for your message',
+        html: '<p>Thank you for your message</p>'
+    }
+    emailService.sendEmail(confirmationEmailOptions)
     res.json({ message: 'Email sent' })
 })
+/* message in the console server is running */
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)

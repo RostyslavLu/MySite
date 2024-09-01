170cd8cc985ae9bf968dccc2751c53c391af6bdf
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2024-09-01 19:01:24 -0400

    contact social improuvement

--- client/src/components/ContactSocial.vue
+++ client/src/components/ContactSocial.vue
@@ -1,48 +1,31 @@
 <script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub, faLinkedin, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
 </script>
 
 <template>
   <div class="contact-social">
     <div class="contact-social-element">
       <a href="https://github.com/RostyslavLu" target="_blank" aria-label="github">
-        <svg width="36" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-          <path fill-rule="evenodd"
-            d="M12 2c-2.4 0-4.7.9-6.5 2.4a10.5 10.5 0 0 0-2 13.1A10 10 0 0 0 8.7 22c.5 0 .7-.2.7-.5v-2c-2.8.7-3.4-1.1-3.4-1.1-.1-.6-.5-1.2-1-1.5-1-.7 0-.7 0-.7a2 2 0 0 1 1.5 1.1 2.2 2.2 0 0 0 1.3 1 2 2 0 0 0 1.6-.1c0-.6.3-1 .7-1.4-2.2-.3-4.6-1.2-4.6-5 0-1.1.4-2 1-2.8a4 4 0 0 1 .2-2.7s.8-.3 2.7 1c1.6-.5 3.4-.5 5 0 2-1.3 2.8-1 2.8-1 .3.8.4 1.8 0 2.7a4 4 0 0 1 1 2.7c0 4-2.3 4.8-4.5 5a2.5 2.5 0 0 1 .7 2v2.8c0 .3.2.6.7.5a10 10 0 0 0 5.4-4.4 10.5 10.5 0 0 0-2.1-13.2A9.8 9.8 0 0 0 12 2Z"
-            clip-rule="evenodd" />
-        </svg>
+        <FontAwesomeIcon :icon="faGithub" />
         <span>GitHub</span>
       </a>
     </div>
     <div class="contact-social-element">
       <a href="https://www.linkedin.com/in/rostyslav-luchyshyn-66350ba5/" target="_blank" aria-label="linkedin">
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-          <path fill-rule="evenodd"
-            d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z"
-            clip-rule="evenodd" />
-          <path d="M7.2 8.8H4v10.7h3.2V8.8Z" />
-        </svg>
+        <FontAwesomeIcon :icon="faLinkedin" />
         <span>Linkedin</span>
       </a>
     </div>
     <div class="contact-social-element">
       <a href="https://www.facebook.com/rluchyshyn" target="_blank" aria-label="facebook">
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor" viewBox="0 0 24 24">
-          <path fill-rule="evenodd"
-            d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
-            clip-rule="evenodd" />
-        </svg>
+        <FontAwesomeIcon :icon="faFacebook" />
         <span>Facebook</span>
       </a>
     </div>
     <div class="contact-social-element">
       <a href="https://www.instagram.com/rostyslav_l/" target="_blank" aria-label="instagram">
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor" viewBox="0 0 24 24">
-          <path fill="currentColor" fill-rule="evenodd"
-            d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
-            clip-rule="evenodd" />
-        </svg>
+        <FontAwesomeIcon :icon="faInstagram" />
         <span>Instagram</span>
       </a>
     </div>
